a98f63c045cab0d32e8099d9749a21f63c59036a
Paolo <pzilioti@gmail.com>
2016-11-17 16:51:07 -0200

    ajustados slots de tempo

--- server.js
+++ server.js
@@ -22,22 +22,26 @@ router.use(function(req, res, next) {
 });
 
 router.get('/demo', function(req, res){
+	var endAt = Date.now()
+	var startAt = moment(endAt).subtract(60, "minutes").valueOf()   	
 	request
 	   .get('localhost:3001/visits-count')
-	   .query({ startAt: "1479395689874", endAt: "1479402393637" })
+	   .query({ startAt: startAt, endAt: endAt })
 	   .end(function(err, response){
 
 		var text = JSON.parse(response.text)
 		var result = text.result
 console.log('result', result)
 		var maxVisits = 0
+		var countTotal = 0
 		result.summary.forEach(function(slot){
 		  if (slot.totalPeopleCount > maxVisits) maxVisits = slot.totalPeopleCount
+		  countTotal = countTotal + slot.occupationCount;
 		})
 		var chart = result.summary.map(function(slot, idx){
 		    var computedHeight = (slot.totalPeopleCount * 100) / maxVisits 
 		    computedHeight = computedHeight.toFixed(2) || 0
-		    var time = moment(slot.start).format('LT')
+		    var time = moment(slot.end).format('LT')
 		    var style = "width: 40px; display: inline-block; margin-right: 10px;"
 		    return (
 			'<div class="slot" style="'+style+'">'+
@@ -49,7 +53,18 @@ console.log('result', result)
 		var barsContent = ''
 		chart.forEach(function(el){barsContent+=el})
 		var style = "white-space: nowrap; height: 200px; max-width: 860px; display: inline-block; background-color: #f3f2f2; padding: 20px; overflow-x: auto; overflow-y: hidden;"
-		var htmlBody = '<html><body><div id="chart" style="'+style+'">'+barsContent+'</div></body></html>'
+		var headContent = '<head>'+
+		    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>'+
+			'<script>'+
+			'function load() {'+
+			    'setTimeout(function(){'+
+			        'location.reload()'+
+			    '},30*1000)'+
+		      	'}'+	
+		      	'window.onload = load;'+
+		'</script>'+
+		'</head>'
+		var htmlBody = '<html>'+headContent+'<body>Ocupação de pessoas na última hora<br><div id="chart" style="'+style+'">'+barsContent+'</div><div id="info">Pessoas dentro do ambiente:'+countTotal+'</div></body></html>'
     		res.send(htmlBody);
 	   });
 });
@@ -86,7 +101,7 @@ router.get('/visits-count', function(req, res){
 			
 			var rangeSize = endAtTimestamp - startAtTimestamp
 			// var slotsCount = rangeSize / (60 *60 * 1000) // 1hour
-			var slotSize = (5 *60 * 1000) // 1 minute
+			var slotSize = (5 *60 * 1000) // 5 minute
 			var slotsCount = rangeSize / slotSize
 			var roundedSlots = parseInt(slotsCount)
 			var slots = []
@@ -94,7 +109,7 @@ router.get('/visits-count', function(req, res){
 				var slot = Object.assign({}, slotMock)
 				var events = []
 				slot.events = events
-				slot.start = (i == 1 ? startAtTimestamp : startAtTimestamp + (i * slotSize) + 1 )
+				slot.start = (i == 1 ? startAtTimestamp : startAtTimestamp + ((i-1) * slotSize) + 1 )
 				slot.end = (i == roundedSlots ? endAtTimestamp : slot.start + slotSize )
 				slot.id = slot.start+'-'+slot.end
 				//console.log('slot========',slot)
